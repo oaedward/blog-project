4fd7a97272e95446bbf0661f252cfb030a5c20ce
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-11-05 05:14:06 +0000

    redesigned post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,18 @@
 class Post < ApplicationRecord
-    validates :title, presence: true
+    validates :title, :image, presence: true
     belongs_to :category
     has_rich_text :content
-    has_one_attached :image
+    has_one_attached :image, :dependent => :destroy
+
+#     private
+
+#     def image_type
+#         if image.attached? == false
+#             errors.add(:image, "is missing!")
+#         end
+#         if !image.content_type.in?(%('image/jpeg image/png'))
+#             errors.add(:image, "needs to be a jpeg or png")
+#         end
+#     end
+    
 end
