85cb82cbb38e75fc003bcc3e249b83a1f53826dd
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-11-09 14:57:44 +0000

    added bio to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_085837) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_133135) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_085837) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "views", default: 0
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
