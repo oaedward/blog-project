f531c089c18c38915364c3f6836abe92b927a0ce
Edward Opoku-Agyemang <oaedward83@gmail.com>
2022-11-09 16:30:31 +0000

    add location to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_154740) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_09_155633) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_09_154740) do
     t.text "bio"
     t.string "profession"
     t.string "work"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
